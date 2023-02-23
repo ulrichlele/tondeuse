de8ca5e8ef2d63f7782cd301152d4eae2fa06e23
Ulrich LELE <lele.ulrich2@gmail.com>
2023-02-23 10:01:14 +0100

    add main app cli

--- src/main/java/io/lele/mowitnow/tondeuse/adapter/cli/App.java
+++ src/main/java/io/lele/mowitnow/tondeuse/adapter/cli/App.java
@@ -9,8 +9,25 @@ import io.lele.mowitnow.tondeuse.domain.command.LawnMowing;
  * @author Ulrich LELE
  */
 public class App {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        if(args.length <1)
+            throw new RuntimeException("Argument file path not present");
+        String filePathArgument = args[0];
+        if(filePathArgument.contains("=")){
+            if(filePathArgument.split("=").length <2){
+                throw new RuntimeException("Argument file path not present");
+            }else{
+                filePathArgument = filePathArgument.split("=")[1];
+            }
+        }
+        try {
+            LawnMowing lawnMowing = MowerFileReader.buildLawnMowing(filePathArgument);
+            // System.out.println("lawnMowing :"+lawnMowing);
+            LawnMowerUseCase mowerUseCase = new LawnMower();
+            mowerUseCase.mow(lawnMowing);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
